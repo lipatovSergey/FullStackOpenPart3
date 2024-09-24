31d3ff6a4360f316699887d018d12e53c78385a9
Sergey <lipatovsergey01@gmail.com>
2024-09-24 23:47:05 +0300

    part 3 finished

--- models/person.js
+++ models/person.js
@@ -8,8 +8,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 3,
+    minLength: 8,
     required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
   },
 })
 
